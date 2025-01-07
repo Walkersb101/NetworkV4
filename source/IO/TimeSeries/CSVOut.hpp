4657dffd1f97a2ff0707fe71d07ccd62f4b09965
Sam Walker <walker.sb101@gmail.com>
2025-01-07 12:58:12 +0000

    Clear File on creation

--- source/IO/TimeSeries/CSVOut.hpp
+++ source/IO/TimeSeries/CSVOut.hpp
@@ -7,22 +7,32 @@
 #include <range/v3/view/drop_last.hpp>
 #include <range/v3/view/join.hpp>
 
-#include "IO/TimeSeries/DataOut.hpp"
 #include "IO/BaseIO.hpp"
+#include "IO/TimeSeries/DataOut.hpp"
 
 namespace IO
 {
 namespace timeSeries
 {
-class CSVOut : public fileIO, public timeSeriesOut
+class CSVOut
+    : public fileIO
+    , public timeSeriesOut
 {
 public:
   CSVOut(const std::filesystem::path& _filePath)
-      : fileIO(_filePath), timeSeriesOut()
+      : fileIO(_filePath)
+      , timeSeriesOut()
   {
     if (_filePath.extension() != ".csv") {
-        throw std::invalid_argument("File extension must be .csv");
+      throw std::invalid_argument("File extension must be .csv");
     }
+    std::ofstream file(m_filePath, std::ios::out);
+    if (!file.is_open()) {
+      throw std::runtime_error(
+          "Cannot open file: "
+          + m_filePath.string());  // TODO: maybe this shouldnt just throw
+    }
+    file.close();
   }
   ~CSVOut() override = default;
 
@@ -34,7 +44,7 @@ public:
 
 private:
   void writeRow(const std::vector<writeableTypes>& _row,
-                       std::ios::openmode _mode)
+                std::ios::openmode _mode)
   {
     std::ofstream file(m_filePath, _mode);
     if (!file.is_open()) {
